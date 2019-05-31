76e46bd1b43286ea993aa7d41a76adce60a484a6
Johnson <j3.soon@msa.hinet.net>
2019-06-01 01:14:22 +0800

    Add state: game

---
+++ src/states/game.ts
@@ -0,0 +1,10 @@
+import * as Assets from '../assets';
+
+export default class Game extends Phaser.State {
+  public create(): void {
+  }
+  public update(): void {
+  }
+  public postUpdate(): void {
+  }
+}
