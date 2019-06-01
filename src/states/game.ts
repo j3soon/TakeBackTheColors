4161921352943ae003ba5ddc937ae6b3e77f2cd1
Johnson <j3.soon@msa.hinet.net>
2019-06-01 23:04:39 +0800

    Add multi-layer background

--- src/states/game.ts
+++ src/states/game.ts
@@ -1,16 +1,18 @@
 import * as Assets from '../assets';
 import PlayerObject from '../objects/playerObject';
 import MapObject from '../objects/mapObject';
-// import Background from '../objects/background';
+import BackgroundObject from '../objects/backgroundObject';
 
 export default class Game extends Phaser.State {
   private readonly gravity = 1800;
-  private playerObj: PlayerObject;
+  private bgObj: BackgroundObject;
   private mapObj: MapObject;
+  private playerObj: PlayerObject;
 
   // TODO: Fix Tunneling (Bullet-Through-Paper) problem by CCD.
   public create(): void {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.bgObj = new BackgroundObject(this.game);
     this.mapObj = new MapObject(this.game);
     this.playerObj = new PlayerObject(this.game, 64, 3072, this.gravity);
     this.game.camera.follow(this.playerObj.getPlayer(), Phaser.Camera.FOLLOW_LOCKON, 1, 1);
